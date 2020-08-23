735eed31e3683cc0a41d666c527d4fc24f5f5228
Payal Bedare <68193580+Payal-Bedare@users.noreply.github.com>
2020-08-23 11:37:45 +0530

    Add files via upload

---
+++ 02_MyWindowCases/WindowCases/Source.cpp
@@ -0,0 +1,83 @@
+//Headers
+#include<windows.h>
+
+//global function declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//variable declarations
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+	//code
+	//initialization of WINDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//register above class
+	RegisterClassEx(&wndclass);
+
+	//create window
+	hwnd = CreateWindow(szAppName,
+		TEXT("Payal..Window Cases"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//message loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//code
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		MessageBox(hwnd, TEXT("wm_create is received"), TEXT("msg"), MB_ICONINFORMATION |MB_OK);
+		break;
+	case WM_KEYDOWN:
+		MessageBox(hwnd, TEXT("wm_keydown is received"), TEXT("msg"), MB_OKCANCEL);
+		break;
+	case WM_LBUTTONDOWN:
+		MessageBox(hwnd, TEXT("wm_Lbutton is received"), TEXT("msg"), MB_OK);
+		break;
+	case WM_RBUTTONDOWN:
+		MessageBox(hwnd, TEXT("wm_Rbutton is received"), TEXT("msg"), MB_OK);
+		break;
+
+	case WM_DESTROY:
+		MessageBox(hwnd, TEXT("wm_destroy is received"), TEXT("msg"), MB_OK);
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
