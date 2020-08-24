4b4d7b70944b23c464cf5efcd511851116439f80
Payal Bedare <68193580+Payal-Bedare@users.noreply.github.com>
2020-08-24 10:52:09 +0530

    Add files via upload

---
+++ Server/def/ByIDE/MyMath/MyMath.cpp
@@ -0,0 +1,26 @@
+#include<Windows.h>
+#include"MyMath.h"
+//entry point function
+BOOL WINAPI DllMain(HINSTANCE hDll, DWORD dwReason, LPVOID lpReserved){
+	//code
+	switch (dwReason) {
+	case DLL_PROCESS_ATTACH :		//dll Injection
+		break;
+	case DLL_THREAD_ATTACH :
+		break;
+	case DLL_THREAD_DETACH :
+		break;
+	case DLL_PROCESS_DETACH :
+		break;
+	default :
+		break;
+	}
+	return(TRUE);
+}
+
+//exported function definition
+ extern "C" int MakeSquare(int num) {
+	//code
+	return num * num;
+}
+
